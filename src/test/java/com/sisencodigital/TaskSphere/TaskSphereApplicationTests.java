269bfc2837a37648f96a2d15d0fc76b53c559ada
heshan <heshansandakelum2018@gmail.com>
2026-07-07 08:08:10 +0530

    initialized project with dependencies

---
+++ src/test/java/com/sisencodigital/TaskSphere/TaskSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisencodigital.TaskSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
