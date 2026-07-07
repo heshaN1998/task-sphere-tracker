48d444f443cc3a1ac71c3a1893731e48763f6478
heshan <heshansandakelum2018@gmail.com>
2026-07-07 13:53:58 +0530

    completed Repository layer with abstract method and JPQL Queries

--- src/main/java/com/sisencodigital/TaskSphere/repositories/ProjectRepository.java
+++ src/main/java/com/sisencodigital/TaskSphere/repositories/ProjectRepository.java
@@ -3,5 +3,10 @@ package com.sisencodigital.TaskSphere.repositories;
 import com.sisencodigital.TaskSphere.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+    Optional<Project> findByName(String name);
+    boolean existsByName(String name);
 }
+
--- src/main/java/com/sisencodigital/TaskSphere/repositories/WeekProjRepository.java
+++ src/main/java/com/sisencodigital/TaskSphere/repositories/WeekProjRepository.java
@@ -1,8 +1,48 @@
 package com.sisencodigital.TaskSphere.repositories;
 
+import com.sisencodigital.TaskSphere.entities.ReportStatus;
 import com.sisencodigital.TaskSphere.entities.WeeklyReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface WeekProjRepository extends JpaRepository<WeeklyReport,Long>, JpaSpecificationExecutor<WeeklyReport> {
+    List<WeeklyReport> findByUserId(Long userId);
+    Optional<WeeklyReport> findByUserIdAndWeekStartDate(Long userId,LocalDate weekStartDate);
+    List<WeeklyReport> findByWeekStartDateAndReportStatus(LocalDate weekStartDate, ReportStatus status);
+    long countByWeekStartDateAndReportStatus(LocalDate weekStartDate, ReportStatus reportStatus);
+
+    @Query("SELECT COUNT(DISTINCT r.user.id)" +
+            "FROM WeeklyReport r " +
+            "WHERE r.weekStartDate=:weekStart " +
+            "AND r.reportStatus=:status")
+    long countUsersByWeekStartDateAndReportStatus(@Param("weekStart") LocalDate weekStart, @Param("status") ReportStatus Status);
+
+    @Query("SELECT r FROM WeeklyReport r " +
+            "WHERE r.weekStartDate=:weekStart")
+    List <WeeklyReport> findAllForWeek(@Param("weekStart") LocalDate weekStart);
+
+    @Query("SELECT r.project.name,COUNT(r) " +
+            "FROM WeeklyReport r"+ " WHERE r.weekStartDate " +
+            "BETWEEN :start and :end AND r.project is not null " +
+            " GROUP BY r.project.name")
+    List<Object[]> countReportsGroupedByProject(@Param("start") LocalDate Start,@Param("end") LocalDate end);
+
+    @Query("SELECT r.weekStartDate,COUNT(r) FROM WeeklyReport r"+" WHERE r.reportStatus=:status and (:userId is null or r.user.id=:userId) "+"AND r.weekStartDate BETWEEN :start AND :end " + " Group By r.weekStartDate ORDER BY r.weekStartDate ASC")
+    List<Object[]> tasksCompletedTrend(@Param("userId") Long userId,
+                                       @Param("status") ReportStatus reportStatus,
+                                       @Param("start") LocalDate start,
+                                       @Param("end") LocalDate end);
+
+
+    @Query("SELECT r FROM WeeklyReport r " +
+            "WHERE r.bugs is not null and r.bugs <>'' " +
+            "AND r.weekStartDate=:weekStart")
+    List <WeeklyReport> findOpenBugsForWeek(@Param("weekStart") LocalDate weekStart);
 }
+
