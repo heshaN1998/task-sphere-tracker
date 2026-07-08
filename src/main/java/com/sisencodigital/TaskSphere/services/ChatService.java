cf25d4336e98ad4b5bdb5a252ff040c7b1ec30c3
heshan <heshansandakelum2018@gmail.com>
2026-07-08 22:28:59 +0530

    completed Ai Assitant sections

--- src/main/java/com/sisencodigital/TaskSphere/services/ChatService.java
+++ src/main/java/com/sisencodigital/TaskSphere/services/ChatService.java
@@ -5,19 +5,33 @@ import com.sisencodigital.TaskSphere.dtos.requestdtos.ChatRequestDTO;
 import com.sisencodigital.TaskSphere.dtos.responcedtos.ChatResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class ChatService {
     private final ChatClient chatClient;
     private final ReportAiTools reportAiTools;
+    private final ChatMemory chatMemory;
+
+    public ChatService(ChatClient.Builder builder,
+                       ReportAiTools reportAiTools,
+                       ChatMemory chatMemory) {
+
+        this.chatClient = builder.build();
+        this.reportAiTools = reportAiTools;
+        this.chatMemory = chatMemory;
+    }
+
 
     public ChatResponseDto ask(ChatRequestDTO requestDTO){
         String responseDTO=chatClient
                 .prompt()
                 .user(requestDTO.getMessage())
                 .tools(reportAiTools)
+                .advisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .call()
                 .content();
                 //WRONG   .tools(reportAiTools);
